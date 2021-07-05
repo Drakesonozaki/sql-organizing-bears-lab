0e41dacb52ee9a6a2678686fb7ca251d23640229
Drakesonozaki <josephortizbw@gmail.com>
2021-07-05 17:35:40 -0600

    commit tree fe0f043dcb4ddfecd16a9759f628aea7cff75e16

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 22, "M", "Black", "playful", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 10, "F", "Brown", "calm", 1);
+INSERT INTO bears VALUES (3, "Tabitha", 9, "F", "White", "calm", 0);
+INSERT INTO bears VALUES (4, "Sargeant Brown", 16, "M", "Brown", "Aggressive", 1);
+INSERT INTO bears VALUES (5, "Melissa", 5, "F", "Red", "Friendly", 1);
+INSERT INTO bears VALUES (6, "Grinch", 7, "M", "Black", "Mean", 1);
+INSERT INTO bears VALUES (7, "Wendy", 21, "F", "White", "Timid", 0);
+INSERT INTO bears VALUES (8,null, 14, "F", "Brown", "Mysterious", 0);
